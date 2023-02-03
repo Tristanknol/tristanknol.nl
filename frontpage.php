711bd4b2ee92a2c58d36426c2bb5d192a68bfefe
Support <support@profi-web.nl>
2023-02-03 11:05:03 +0100

    Commit 3-2-2023

--- frontpage.php
+++ frontpage.php
@@ -2,12 +2,10 @@
 <?php get_header() ?>
 <div class="container-fluid m-0 p-0">
     <img alt="imageplants" class="fpHeader position-absolute" src="<?php echo get_template_directory_uri();?>/assets/media/fpHeader.jpg" width="100%">
-    <div class="position-absolute text-center d-flex h-100 header-text w-100">
-        <h1 class="text-decoration-underline underlinestyle fs-7r m-auto text-white">Tristan Knol</h1>
+    <div class="position-absolute text-center d-flex h-100 content w-100">
+        <h1 class="text-decoration-underline textShadows underlinestyle fs-7r m-auto text-white">Tristan Knol</h1>
         </div>
     </div>
 </div>
-
-<div class=""
 
 <?php get_footer() ?>
